116b2e42ff8cf635a944c8d5d98aaf5cdcad9297
jjying89 <70499413+jjying89@users.noreply.github.com>
2020-11-24 18:08:41 -0500

    update
    update start & start/end in app.py

--- app.py
+++ app.py
@@ -43,8 +43,8 @@ def home():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end><br/>"
+        f"/api/v1.0/&lt;start&gt;<br/>"
+        f"/api/v1.0/&lt;start&gt;/&lt;end&gt;<br/>"
     )
 
 #precipitation data
@@ -64,9 +64,9 @@ def precipitation():
         prcp_dict = {}
         prcp_dict["date"] = date
         prcp_dict["prcp"] = prcp
-        precipitation.append(prcp.dict)
+        precipitation.append(prcp_dict)
 
-    return jsonify(precipitation) #NOT WORKING
+    return jsonify(precipitation)
 
 #list of stations
 @app.route("/api/v1.0/stations")
@@ -74,32 +74,89 @@ def stations():
     
     session = Session(engine)
 
-    stations = session.query(S.station, S.name).all()
+    results = session.query(S.station, S.name).all()
 
     session.close()
 
-    return jsonify(stations)
+    return jsonify(results)
 
 #date and temp data of the most active station for the last year
 @app.route("/api/v1.0/tobs")
 def tobs():
     
+    session = Session(engine)
 
-    return 
+    #most active station is USC00519281
+    #last year date starts from 2016-08-23
 
+    results = session.query(M.date, M.tobs).\
+        filter(M.station == 'USC00519281').\
+        filter(M.date >= '2016-08-23').\
+        order_by(M.date).all()
+
+    session.close()
+
+    temp = []
+
+    for date, tobs in results:
+        tobs_dict = {}
+        tobs_dict["date"] = date
+        tobs_dict["tobs"] = tobs
+        temp.append(tobs_dict)
+
+    return jsonify(temp)
+
+#temp data with a given start date
 @app.route("/api/v1.0/<start>")
-def start():
+def temp_start(start):
     
+    session = Session(engine)
 
-    return 
+    results = session.query(func.min(M.tobs), func.avg(M.tobs), func.max(M.tobs)).\
+        filter(M.date >= start).all()
+        
+    session.close()
 
+    start_results = []
+
+    for tmin, tavg, tmax in results:
+        start_dict = {}
+        start_dict['tmin'] = tmin
+        start_dict['tavg'] = tavg
+        start_dict['tmax'] = tmax
+        start_results.append(start_dict)
+
+    return jsonify(start_results)
+
+#temp data with a given start and end date
 @app.route("/api/v1.0/<start>/<end>")
-def start_end():
+def temp_start_end(start, end):
     
+    session = Session(engine)
 
-    return 
+    results = session.query(func.min(M.tobs), func.avg(M.tobs), func.max(M.tobs)).\
+        filter(M.date >= start).\
+        filter(M.date <= end).all()
+    
+    session.close()
+    
+    start_end_results = []
+
+    for tmin, tavg, tmax in results:
+        start_end_dict = {}
+        start_end_dict['tmin'] = tmin
+        start_end_dict['tavg'] = tavg
+        start_end_dict['tmax'] = tmax
+        start_end_results.append(start_end_dict)
+
+    return jsonify(start_end_results)
 
 
-# 4. Define main behavior
+# Define main behavior
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
